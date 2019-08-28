4e5eae2c132281d6406a74736dc56e65dad34b61
Trevor Gollaher <trevorgollaher@Trevors-Air.fios-router.home>
2019-08-27 21:16:09 -0400

    finished game

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# Mystery-word-game
-A mystery word guessing game made with Ruby
+
+## Getting started
+
+1. Fork
+1. Create a feature branch
+1. Clone
+1. Pull Request
+
+# Mystery Word Game
+
+Let's build a mystery word game in Ruby!
+
+In Mystery word:
+
+*   One player sets a MyStERy WoRD at the beginning of the game
+*   The game shows the current mystery word, with underscores showing for each character that is still a mystery.
+*   The second player guesses one letter in each round
+*   If the letter is in the word, that letter is revealed in the word
+*   If the letter is not in the word, the player loses one life
+
+Here's what a game of Mystery Word should look like:
+
+```
+What is the word? Player 2 look away...
+dog
+
+
+
+
+The word is
+___
+You have 3 lives left
+Guess a letter:
+o
+Yup, the word includes that letter!
+
+The word is
+_o_
+You have 3 lives left
+Guess a letter:
+r
+Oh no! The word doesn't include that letter!
+
+The word is
+_o_
+You have 2 lives left
+Guess a letter:
+d
+Yup, the word includes that letter!
+
+The word is
+do_
+You have 2 lives left
+Guess a letter:
+g
+Yup, the word includes that letter!
+You won! The word is
+dog
+```
+
+## Some technical requirements
+
+*   The classes and methods you'll need are already defined in `main.rb`. It's your job to implement each method.
+*   You __will__ need to create new instances variables.
+*   Don't add any more code to global scope.
+
+## Tip
+
+* Ask for user input by using `gets.chomp`
+
+## Bonus
+
+*   Validate the user's input. If the user enters a non-alphabetical character, ask them to.
+*   If the user enters more than one letter at a time, tell them that's not okay.
+*   Let the players run the game again.
--- main.rb
+++ main.rb
@@ -1,20 +1,42 @@
 class Word
-  # Stores the provided word in an instance variable and
-  # setups up any other instance variables.
+  attr_accessor :displaying_word  
   def initialize(word)
+    @word = word
+    @array_of_letters = @word.split("")
+    @displaying_word = "_" * word.length
+
   end
 
-  # Guess a letter in the word.
+    # Guess a letter in the word.
   # Should return a boolean. true if the letter is in the word, false if not.
   def guess?(letter)
+    if (@word.include?(letter))
+      arr_of_indicies = []
+      arr_of_indicies = @array_of_letters.each_index.select do |i|
+        @array_of_letters[i] == letter
+      end
+      arr_of_indicies.each do |index|
+        @displaying_word[index] = letter
+      end
+      return true
+    else
+      return false
+    end
   end
 
   # Whether all letters in the word have been guessed correctly.
   def guessed_all_correct?
+    if @displaying_word == @word
+      puts "Nailed it"
+      return true
+    else
+      return false
+    end
   end
 
   # Display the current state of the guessed word for the player.
   def output_word
+    puts @displaying_word
   end
 
   # Check whether a provided character is a letter
@@ -23,16 +45,40 @@ class Word
 end
 
 class MysteryWordGame
+  attr_accessor :word
   # Initialize any state in the game, and start the game loop.
   def initialize
+    @guesses = 3
+    @word
   end
 
   def ask_for_word
+    puts "Enter your word"
+    user_word = gets.chomp
+    @word = Word.new(user_word) 
   end
 
   # Run the game loop, which continues until the player wins or loses.
   def game_loop
+    game_over = false
+    while @guesses > 0 && game_over == false do
+      puts "Enter letter"
+      user_letter = gets.chomp
+      if @word.guess?(user_letter)
+        @word.output_word
+      else
+        @guesses -=1
+        puts "You have #{@guesses} remaining"
+        if @guesses == 0
+          puts "Game Over"
+        end
+      end
+      game_over = @word.guessed_all_correct?
+    end
   end
 end
 
-MysteryWordGame.new
+
+game = MysteryWordGame.new
+game.ask_for_word
+game.game_loop
